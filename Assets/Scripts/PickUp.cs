c7a2c9209856c5f767d897719f2536d412c0e29a
Juan_Pablo <chumi1612@hotmail.com>
2017-03-20 18:14:19 -0500

    Primera Version!

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public int distanceToItem;
+    public GameObject carta;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Collect();
+	}
+
+    void Collect ()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(rayo,out hit,distanceToItem))
+            {
+                if (hit.collider.gameObject == carta)
+                {
+                    Debug.Log("PickUp");
+                    carta.SetActive(false);
+                    
+                }
+            }
+        }
+    }
+}
